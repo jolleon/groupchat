9c6a48e559d4f44aa18cbcd122274c5b6cd2874f
Jules Olleon <jolleon@gmail.com>
2022-12-02 21:09:03 +0100

    minimal cleanup

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, request, redirect, render_template, session
 from twilio.twiml.messaging_response import MessagingResponse
 
-
-import db
-import twilio_api
+import logic
 
 
 app = Flask(__name__)
@@ -13,19 +11,14 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # TODO env or generate
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
-
-    print(request.values)
-
     from_number = request.values.get('From')
     to_number = request.values.get('To')
     body = request.values.get('Body')
-
     print(from_number, to_number, body)
 
-    db.process_message(from_number, to_number, body)
-    # Start our TwiML response
-    resp = MessagingResponse()
+    logic.process_message(from_number, to_number, body)
 
+    resp = MessagingResponse()
     return str(resp)
 
 
@@ -33,12 +26,12 @@ def sms_reply():
 def index():
     if 'user_id' not in session:
         return redirect('/login')
-    user = db.get_user(session["user_id"])
+    user = logic.get_user(session["user_id"])
     if user is None:
         # something weird - user got deleted? make them login again
         return redirect('/logout')
-    all_chats = db.get_all_chats()
-    my_chats = db.get_user_chats(user.id)
+    all_chats = logic.get_all_chats()
+    my_chats = logic.get_user_chats(user.id)
     return render_template('index.html', user=user, all_chats=all_chats, my_chats=my_chats)
 
 
@@ -48,7 +41,7 @@ def login():
     if request.method == 'POST':
         name = request.form['username']
         phone = request.form['userphone']
-        user = db.login_or_create_user(name, phone)
+        user = logic.login_or_create_user(name, phone)
         session['user_id'] = user.id
         return redirect('/')
     return '''
@@ -62,17 +55,27 @@ def login():
 
 @app.route('/chats', methods=['POST'])
 def create_chat():
+    # This endpoint is a bit overloaded - if we had a proper JS frontend
+    # I'd cleanup the API to something like:
+    # POST /chats: create new chat
+    # POST /me/chats {chat_id: 123}: join a chat
+    # DELETE /me/chats {chat_id: 123}: leave a chat
+    # (for instance... could be /me or /users/{id} or... one of the many options)
+    # Also implement auth so you can't join/leave chats for someone else etc.
+    # But for now we just look at the form variable provided to decide what we need to do.
     chat_id = request.form.get('chatid')
     membership_id = request.form.get('membershipid')
     if chat_id is not None:
-        user = db.get_user(session['user_id'])
-        db.join_chat(user, chat_id)
+        # join an existing chat
+        user = logic.get_user(session['user_id'])
+        logic.join_chat(user, chat_id)
     elif membership_id is not None:
-        db.leave_chat(membership_id)
+        # leave an existing chat
+        logic.leave_chat(membership_id)
     else:
         # we're creating a new chat
         name = request.form['chatname']
-        db.create_chat(name)
+        logic.create_chat(name)
     return redirect('/')
 
 
--- db.py
+++ db.py
@@ -1,8 +1,6 @@
 import sqlite3
 from collections import namedtuple
 
-import twilio_api
-
 
 con = sqlite3.connect("groupchat.db", check_same_thread=False)
 cur = con.cursor()
@@ -14,116 +12,13 @@ TWILIO_PHONES = [
     '+17208079029'
 ]
 
+# Very basic "models" just to make it a bit nicer to use than sqlite's indexing
 User = namedtuple('User', ['id', 'phone', 'name'])
 Chat = namedtuple('Chat', ['id', 'name'])
 JoinedChat = namedtuple('JoinedChat', ['membership_id', 'chat_id', 'group_name', 'twilio_phone'])
 
 
-def login_or_create_user(username, userphone):
-    user = cur.execute("select * from users where phone = ?", (userphone,)).fetchone()
-    # TODO what to do if username is different?
-    # we could surface it or just update it to be the new value
-    print(user)
-    if user is None:
-        return User(create_user(username, userphone), username, userphone)
-    else:
-        return User._make(user)
-
-
-def create_user(username, userphone):
-    cur.execute("insert into users (phone, name) values (?, ?)", (userphone, username))
-    con.commit()
-    return cur.lastrowid
-
-
-def get_user(user_id):
-    u = cur.execute("select * from users where id = ?", (user_id,)).fetchone()
-    print(u)
-    if u is None:
-        return None
-    return User._make(u)
-
-
-def get_all_chats():
-    return map(Chat._make, cur.execute("select * from chats").fetchall())
-
-
-def get_user_chats(user_id):
-    return map(JoinedChat._make,
-        cur.execute("""
-            select memberships.id, chats.id, chats.name, memberships.twilio_phone from chats
-            join memberships on memberships.chat_id = chats.id
-            where memberships.user_id = ?
-        """, (user_id,)).fetchall()
-    )
-
-
-def create_chat(name):
-    cur.execute("insert into chats (name) values (?)", (name,))
-    con.commit()
-    return cur.lastrowid
-
-
-def join_chat(user, chat_id):
-    print(f"user {user} wants to join chat {chat_id}")
-
-    user_id = user.id
-
-    # don't join a chat twice
-    already_joined = cur.execute("select twilio_phone from memberships where user_id = ? and chat_id = ?", (user_id, chat_id)).fetchone()
-    if already_joined is not None:
-        print("already joined this chat")
-        return
-
-    # find free twilio number
-    used_numbers = cur.execute("select twilio_phone from memberships where user_id = ?", (user_id,)).fetchall()
-    used_numbers = [n[0] for n in used_numbers]
-    print('already used numbers:')
-    print(used_numbers)
-    twilio_number = None
-    for number in TWILIO_PHONES:
-        if number not in used_numbers:
-            twilio_number = number
-
-    print(twilio_number)
-    if twilio_number is None:
-        print("already using all phone numbers!")
-        raise Exception("no free twilio number")
-
-    cur.execute("insert into memberships (user_id, chat_id, twilio_phone) values (?, ?, ?)", (user_id, chat_id, twilio_number))
-    con.commit()
-
-    group_name = cur.execute("select name from chats where id = ?", (chat_id,)).fetchone()[0]
-    twilio_api.send_welcome_message(user.phone, twilio_number, group_name)
-
-
-def leave_chat(membership_id):
-    cur.execute("delete from memberships where id = ?", membership_id)
-    con.commit()
-
-
-def process_message(from_number, to_number, message):
-    chat_id, sender_name = cur.execute("""
-        select memberships.chat_id, users.name from memberships
-        join users on users.id = memberships.user_id
-        where users.phone = ? and memberships.twilio_phone = ?
-    """, (from_number, to_number)).fetchone()
-    print(f"message from user {sender_name} to group {chat_id}")
-
-    to_users = cur.execute("""
-        select memberships.twilio_phone, users.phone from memberships
-        join users on users.id = memberships.user_id
-        where memberships.chat_id = ?
-    """, (chat_id,)).fetchall()
-
-    print(to_users)
-
-    for twilio_number, user_number in to_users:
-        print(f"sending sms to {user_number} from {twilio_number}")
-        twilio_api.send_message(user_number, twilio_number, f"{sender_name}: {message}")
-
-
-
+# DB tables creation
 def setup():
     cur.execute("""
     CREATE TABLE users (
